5f57575a267687373212f4b0c1c4f43ced89e7e9
Chin-Ho Lin <tainvecs@gmail.com>
2022-06-22 21:08:57 +0800

    chore: update main page README.md

--- meilisearch/scripts/install.sh
+++ meilisearch/scripts/install.sh
@@ -1,5 +1,5 @@
 GARAGE_MEILISEARCH_ROOT="$(dirname $(cd $(dirname $0) >/dev/null 2>&1; pwd -P;))"
 
 # install meilisearch
-pushd "$GARAGE_MEILISEARCH_ROOT/bin"
+cd "$GARAGE_MEILISEARCH_ROOT/bin"
 curl -L https://install.meilisearch.com | sh
