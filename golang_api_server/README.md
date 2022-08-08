43b1549971979b7c2d470419d456342a255ce837
Chin-Ho, Lin <tainvecs@gmail.com>
2022-08-08 17:25:21 +0800

    chore: golang api server (init es dao) (#2 and #7)
    * feat: add README.md, .gitignore and .env template
    
    * chore: add go.mod and go.sum
    
    * feat: init elasticsearch data access object
    
    * chore: add TODO

---
+++ golang_api_server/README.md
@@ -0,0 +1,16 @@
+# Golang API Server
+
+## Create a New Golang Project
+```sh
+go mod init api-server
+```
+
+## TODO
+- [x] Create New a Golang Project
+- [ ] HTTP: `net/http` and `github.com/gin-gonic/gin`
+- [ ] Database: `github.com/go-gorm/gorm`
+- [ ] Elasticsearch: `github.com/elastic/go-elasticsearch/v8`
+  - [x] elasticsearch DAO implementation
+  - [ ] elasticsearch DAO test
+  - [ ] elasticsearch service
+  - [ ] elasticsearch handler
---
+++ golang_api_server/pkg/data_access/elasticsearch/search_response.go
@@ -0,0 +1,33 @@
+package es_dao
+
+import "encoding/json"
+
+// response
+type RawSearchResponse struct {
+	Took     int  `json:"took"`
+	TimedOut bool `json:"timed_out"`
+	Shards   struct {
+		Total      int `json:"total"`
+		Successful int `json:"successful"`
+		Skipped    int `json:"skipped"`
+		Failed     int `json:"failed"`
+	}
+	Hits struct {
+		Total struct {
+			Value    int    `json:"value"`
+			Relation string `json:"relation"`
+		} `json:"total"`
+		MaxScore float32           `json:"mas_score"`
+		Hits     []json.RawMessage `json:"hits"`
+	} `json:"hits"`
+	Aggregations json.RawMessage `json:"aggregations,omitempty"`
+	Highlight    json.RawMessage `json:"highlight,omitempty"`
+}
+
+// hit
+type RawSearchHit struct {
+	Index string  `json:"_index"`
+	Type  string  `json:"_type"`
+	ID    string  `json:"_id"`
+	Score float32 `json:"_score"`
+}
