15a2aeb6c45a965e77f4c1380fa358c4307121e7
Chin-Ho Lin <tainvecs@gmail.com>
2022-08-10 17:15:14 +0800

    chore: new install script

---
+++ elasticsearch/scripts/install.sh
@@ -0,0 +1,24 @@
+GARAGE_ELASTICSEARCH_ROOT="$(dirname $(cd $(dirname $0) >/dev/null 2>&1; pwd -P;))"
+
+
+ES_VERSION=8.3.3
+
+
+OS_NAME=`uname`
+
+if [[ $OS_NAME = "Darwin" ]]; then
+
+    curl -O "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$ES_VERSION-darwin-x86_64.tar.gz"
+    curl "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$ES_VERSION-darwin-x86_64.tar.gz.sha512" | shasum -a 512 -c -
+    tar -xzf "elasticsearch-$ES_VERSION-darwin-x86_64.tar.gz"
+    cd "elasticsearch-$ES_VERSION/"
+
+elif [[ $OS_NAME = "Linux" ]]; then
+
+    wget "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$ES_VERSION-linux-x86_64.tar.gz"
+    wget "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-$ES_VERSION-linux-x86_64.tar.gz.sha512"
+    shasum -a 512 -c "elasticsearch-$ES_VERSION-linux-x86_64.tar.gz.sha512"
+    tar -xzf "elasticsearch-$ES_VERSION-linux-x86_64.tar.gz"
+    cd "elasticsearch-$ES_VERSION/"
+
+fi
