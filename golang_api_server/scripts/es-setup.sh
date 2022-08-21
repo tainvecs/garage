a10fee4b7f164f549ad0cf74d4935057d28eff00
Chin-Ho Lin <tainvecs@gmail.com>
2022-08-21 23:06:59 +0800

    refactor: change env ES_HOST -> ES_URL

--- golang_api_server/scripts/es-setup.sh
+++ golang_api_server/scripts/es-setup.sh
@@ -5,13 +5,13 @@ GARAGE_GOLANG_API_SERVER_ROOT="$(dirname $(cd $(dirname $0) >/dev/null 2>&1; pwd
 
 
 # create index
-curl -XPUT "$ES_HOST/$ES_NEWS_DOC_INDEX" \
+curl -XPUT "$ES_URL/$ES_NEWS_DOC_INDEX" \
      -H 'Content-Type: application/json' \
      -d "@$GARAGE_GOLANG_API_SERVER_ROOT/resources/es-news_category.schema.json"
 
 
 # add index aliases
-curl -XPOST "$ES_HOST/_aliases" -H "Content-Type: application/json" -d'
+curl -XPOST "$ES_URL/_aliases" -H "Content-Type: application/json" -d'
 {
    "actions": [
        { "add": {
@@ -28,8 +28,8 @@ curl -XPOST "$ES_HOST/_aliases" -H "Content-Type: application/json" -d'
 
 
 # # index test docs
-# curl -XPOST "$ES_HOST/$ES_NEWS_DOC_INDEX_ALIAS/_bulk" \
+# curl -XPOST "$ES_URL/$ES_NEWS_DOC_INDEX_ALIAS/_bulk" \
 #      -H 'Content-Type: application/json' \
 #      --data-binary "@$GARAGE_GOLANG_API_SERVER_ROOT/data/test-news_category.es.json"
 
-# curl -XPOST "$ES_HOST/_refresh?pretty"
+# curl -XPOST "$ES_URL/_refresh?pretty"
