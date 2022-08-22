7c84cca6282a8a624d738fdfe3e82b145f41db3a
Chin-Ho Lin <tainvecs@gmail.com>
2022-08-22 16:29:02 +0800

    feat: check update and delete result using search and fix typo

--- golang_api_server/pkg/services/news_doc_service/es_dao_test.go
+++ golang_api_server/pkg/services/news_doc_service/es_dao_test.go
@@ -1,22 +1,40 @@
 package news_doc_service
 
 import (
+	"api-server/pkg/utils"
 	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
+var esTestDocID = "test-doc-id"
+
+// test doc for: index, search and delete
+var esTestDoc = NewsDoc{
+	ID:          esTestDocID,
+	Link:        "https://test_news_doc_1",
+	Title:       "test doc title",
+	Description: "test description",
+	Authors:     []string{"a1", "a2"},
+	Category:    "CATEGORIY",
+}
+
+// test doc for: update
+var esUpdateTestDoc = NewsDoc{
+	ID:          esTestDocID,
+	Title:       "updated doc title",
+	Description: "updated description",
+}
+
 func TestESDAOIndex(t *testing.T) {
 
 	fmt.Println("Test services/news_doc_service/es_dao_index.go")
-	fmt.Println("> Index(ctx context.Context, doc *NewsDoc)")
+	fmt.Println("> Index(ctx context.Context, doc *NewsDoc) error")
 
 	// check if env missing
 	esURL := os.Getenv("ES_URL")
@@ -38,25 +56,14 @@ func TestESDAOIndex(t *testing.T) {
 
 	// start indexing
 	ctx := context.Background()
-	timeNow := time.Now()
-
-	doc := NewsDoc{
-		ID:          "test-doc-id",
-		Link:        "https://test_news_doc_1",
-		Title:       "test doc title",
-		Description: "test description",
-		Authors:     []string{"a1", "a2"},
-		Category:    "test_category",
-		CreatedAt:   &timeNow,
-	}
-	err = esDAO.Index(ctx, &doc)
+	err = esDAO.Index(ctx, &esTestDoc)
 	assert.NoError(t, err)
 }
 
 func TestESDAOSearch(t *testing.T) {
 
 	fmt.Println("Test services/news_doc_service/es_dao_search.go")
-	fmt.Println("> Search(ctx context.Context, query string)")
+	fmt.Println("> Search(ctx context.Context, query string) (*ESDAOSearchResponse, error)")
 
 	// check if env missing
 	esURL := os.Getenv("ES_URL")
@@ -80,20 +87,42 @@ func TestESDAOSearch(t *testing.T) {
 	ctx := context.Background()
 	resp, err := esDAO.Search(
 		ctx,
-		`{"from":0, "size":10, "query": {"match_all": {}}}`,
+		`{"from":0, "size":10, "query": {"term": {"id": "`+esTestDocID+`"}}}`,
 	)
 	assert.NoError(t, err)
-	assert.NotEmpty(t, resp)
+	assert.Equal(t, 1, len(resp.Results))
 
-	mar, err := json.MarshalIndent(resp, "", "\t")
-	assert.NoError(t, err)
-	fmt.Println(string(mar))
+	// check if result correct
+	assert.Equal(
+		t,
+		resp.Results[0].Link,
+		esTestDoc.Link,
+	)
+	assert.Equal(
+		t,
+		resp.Results[0].Title,
+		esTestDoc.Title,
+	)
+	assert.Equal(
+		t,
+		resp.Results[0].Description,
+		esTestDoc.Description,
+	)
+	assert.Empty(
+		t,
+		utils.StringSlicesXOR(resp.Results[0].Authors, esTestDoc.Authors),
+	)
+	assert.Equal(
+		t,
+		resp.Results[0].Category,
+		esTestDoc.Category,
+	)
 }
 
 func TestESDAOUpdate(t *testing.T) {
 
 	fmt.Println("Test services/news_doc_service/es_dao_update.go")
-	fmt.Println("> Update(ctx context.Context, doc *NewsDoc)")
+	fmt.Println("> Update(ctx context.Context, doc *NewsDoc) error")
 
 	// check if env missing
 	esURL := os.Getenv("ES_URL")
@@ -113,22 +142,35 @@ func TestESDAOUpdate(t *testing.T) {
 	esDAO, err := NewESDAO(esURL, esIndexIndex, esSearchIndex)
 	assert.NoError(t, err)
 
-	// start indexing
+	// start updating
 	ctx := context.Background()
-
-	doc := NewsDoc{
-		ID:          "test-doc-id",
-		Title:       "updated doc title",
-		Description: "updated description",
-	}
-	err = esDAO.Update(ctx, &doc)
+	err = esDAO.Update(ctx, &esUpdateTestDoc)
 	assert.NoError(t, err)
+
+	// check update by search
+	resp, err := esDAO.Search(
+		ctx,
+		`{"from":0, "size":10, "query": {"term": {"id": "`+esTestDocID+`"}}}`,
+	)
+	assert.NoError(t, err)
+
+	// check if result correct
+	assert.Equal(
+		t,
+		resp.Results[0].Title,
+		esUpdateTestDoc.Title,
+	)
+	assert.Equal(
+		t,
+		resp.Results[0].Description,
+		esUpdateTestDoc.Description,
+	)
 }
 
 func TestESDAODelete(t *testing.T) {
 
 	fmt.Println("Test services/news_doc_service/es_dao_delete.go")
-	fmt.Println("> Delete(ctx context.Context, docID string)")
+	fmt.Println("> Delete(ctx context.Context, docID string) error")
 
 	// check if env missing
 	esURL := os.Getenv("ES_URL")
@@ -148,8 +190,16 @@ func TestESDAODelete(t *testing.T) {
 	esDAO, err := NewESDAO(esURL, esIndexIndex, esSearchIndex)
 	assert.NoError(t, err)
 
-	// start indexing
+	// start deleting
 	ctx := context.Background()
-	err = esDAO.Delete(ctx, "test-doc-id")
+	err = esDAO.Delete(ctx, esTestDocID)
 	assert.NoError(t, err)
+
+	// check delete by search
+	resp, err := esDAO.Search(
+		ctx,
+		`{"from":0, "size":10, "query": {"term": {"id": "`+esTestDocID+`"}}}`,
+	)
+	assert.NoError(t, err)
+	assert.Equal(t, 0, len(resp.Results))
 }
