b31f0129ab5f562262b8599fa17634e70bad2e83
Chin-Ho Lin <tainvecs@gmail.com>
2022-09-29 21:05:33 +0800

    fix: trimmed ip

--- fail2ban/scripts/fail2ban-sshd_ban_ip_from_denyhosts_denied.zsh
+++ fail2ban/scripts/fail2ban-sshd_ban_ip_from_denyhosts_denied.zsh
@@ -2,23 +2,26 @@ GARAGE_F2B_ROOT="$(dirname $(cd $(dirname $0) >/dev/null 2>&1; pwd -P;))"
 DENYHOST_DENY_FILE=$GARAGE_F2B_ROOT/resources/hosts.deny
 
 i=0
-all_n=$(wc -l < $DENYHOST_DENY_FILE | sed 's/[ \t\n]*//')
+all_n=$(wc -l < $DENYHOST_DENY_FILE)
 while IFS="" read -r in_str; do
 
     ((i+=1))
 
     # reomve comment
-    ip_str=$(echo $in_str | sed 's/#.*//')
+    in_str="$(echo $in_str | sed 's/^#.*//g')"
+
+    # remove sshd prefix
+    ip_str="$(echo $in_str | sed 's/sshd\:\(.*\)/\1/')"
 
     # trim space
-    trimmed_ip_str=$(echo $ip_str | sed 's/[ \t\n]*//')
+    trimmed_ip_str="$(echo $ip_str | sed 's/\s*//g')"
 
     # skip empty
-    if [[ -z $trimmed_ip_str ]]; then
+    if [[ -z "$trimmed_ip_str" ]]; then
         continue
     fi
 
-    sudo fail2ban-client set sshd banip $trimmed_ip_str
+    sudo fail2ban-client set sshd banip "$trimmed_ip_str"
     echo " $i / $all_n: banned $trimmed_ip_str"
 
-done < $DENYHOST_DENY_FILE | sed -r 's/sshd:(.*)/\1/'
+done < $DENYHOST_DENY_FILE
