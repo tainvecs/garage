53dab22a732a40f596d9880683dceaf4a3d1c704
Chin-Ho Lin <tainvecs@gmail.com>
2022-12-01 01:22:26 +0800

    feat: add simple es search logic with simple query string query

---
+++ golang_api_server/pkg/services/news_doc_service/search_logic_es.go
@@ -0,0 +1,49 @@
+package news_doc_service
+
+import (
+	"api-server/pkg/data_access/elasticsearch_data_access"
+)
+
+// es search params and query
+type ESSearchParameters struct {
+	Query string `json:"query"`
+	Page  int    `json:"page"`
+	Limit int    `json:"limit"`
+}
+
+func BuildESSearchQuery(params *ESSearchParameters) (*elasticsearch_data_access.QueryBody, error) {
+
+	// bool query
+	boolQuery := elasticsearch_data_access.BoolQuery{
+		Bool: elasticsearch_data_access.Bool{
+			Should:             make([]interface{}, 0),
+			MinimumShouldMatch: "1",
+		},
+	}
+
+	// simple query string query
+	simpleQueryString := elasticsearch_data_access.SimpleQueryString{
+		Query: params.Query,
+		Fields: []string{
+			"title^3.0",
+			"description^1.0",
+		},
+		DefaultOperator: "and",
+	}
+	simpleQueryStringQuery, err := elasticsearch_data_access.NewSimpleQueryStringQuery(simpleQueryString)
+	if err != nil {
+		return nil, err
+	}
+
+	boolQuery.Bool.Should = append(boolQuery.Bool.Should, simpleQueryStringQuery)
+
+	// result query
+	esQueryBody := elasticsearch_data_access.QueryBody{
+		From:     params.Page * params.Limit,
+		Size:     params.Limit,
+		MinScore: 0.00001,
+		Query:    boolQuery,
+	}
+
+	return &esQueryBody, nil
+}
