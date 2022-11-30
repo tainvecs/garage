f2c7c154b0ed258592b98c6613ba1772d18f4e03
Chin-Ho Lin <tainvecs@gmail.com>
2022-12-01 01:54:10 +0800

    feat: add search logic for api

---
+++ golang_api_server/pkg/services/news_doc_service/search_logic_api.go
@@ -0,0 +1,82 @@
+package news_doc_service
+
+import (
+	"context"
+	"encoding/json"
+)
+
+// api request and response
+type SearchRequest struct {
+	Query string `json:"query" form:"query"`
+	Page  int    `json:"page" form:"page"`
+	Limit int    `json:"limit" form:"limit"`
+}
+
+type SearchResponse struct {
+	Total int                  `json:"total"`
+	Docs  []*SearchResponseDoc `json:"docs"`
+}
+
+type SearchResponseDoc struct {
+	UUID        string   `json:"uuid"`
+	Link        string   `json:"link"`
+	Title       string   `json:"title"`
+	Description string   `json:"description"`
+	Authors     []string `json:"authors"`
+	Category    string   `json:"category"`
+}
+
+// api handling function
+type SearchFunc func(ctx context.Context, request *SearchRequest) (*SearchResponse, error)
+
+func NewSearchFunc(esDAO *ESDAO) SearchFunc {
+	return func(ctx context.Context, request *SearchRequest) (*SearchResponse, error) {
+
+		response := SearchResponse{
+			Total: 0,
+			Docs:  make([]*SearchResponseDoc, 0),
+		}
+
+		// build es search query
+		esSearchParams := ESSearchParameters{
+			Query: request.Query,
+			Page:  request.Page,
+			Limit: request.Limit,
+		}
+		esSearchQuery, err := BuildESSearchQuery(&esSearchParams)
+		if err != nil {
+			return &response, err
+		}
+
+		// run es search
+		mar, err := json.Marshal(esSearchQuery)
+		esSearchQueryStr := string(mar)
+		if err != nil {
+			return &response, err
+		}
+
+		searchResp, err := esDAO.Search(ctx, esSearchQueryStr)
+		if err != nil {
+			return &response, err
+		}
+
+		// prepare response
+		response.Total = searchResp.Total
+
+		for _, d := range searchResp.Results {
+			response.Docs = append(
+				response.Docs,
+				&SearchResponseDoc{
+					UUID:        d.UUID,
+					Link:        d.Link,
+					Title:       d.Title,
+					Description: d.Description,
+					Authors:     d.Authors,
+					Category:    d.Category,
+				},
+			)
+		}
+
+		return &response, nil
+	}
+}
