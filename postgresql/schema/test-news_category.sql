211b58539e1135b0bc13a9c71c8af69d87bae89c
Chin-Ho Lin <tainvecs@gmail.com>
2023-01-08 22:47:04 +0800

    fix: typo

--- postgresql/schema/test-news_category.sql
+++ postgresql/schema/test-news_category.sql
@@ -17,6 +17,6 @@ CREATE TABLE IF NOT EXISTS "news_authors" (
    "id"         INT  PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
    "news_id"    INT,
    "authors_id" INT,
-   CONSTRAINT "news_authors_news_id_fkey" FOREIGN KEY ("news_id") REFERENCE "news"("id") ON DELETE CASCADE,
-   CONSTRAINT "news_authors_authors_id_fkey" FOREIGN KEY ("authors_id") REFERENCE "authors"("id") ON DELETE CASCADE
+   CONSTRAINT "news_authors_news_id_fkey" FOREIGN KEY ("news_id") REFERENCES "news"("id") ON DELETE CASCADE,
+   CONSTRAINT "news_authors_authors_id_fkey" FOREIGN KEY ("authors_id") REFERENCES "authors"("id") ON DELETE CASCADE
 );
