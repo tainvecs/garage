7328189c047cc776757891641a63ac98401d44bc
Chin-Ho Lin <tainvecs@gmail.com>
2023-02-06 08:24:37 +0800

    refactor: rename psql dao struct

--- apisrv/pkg/services/newssvc/psql_dao.go
+++ apisrv/pkg/services/newssvc/psql_dao.go
@@ -3,38 +3,38 @@ package newssvc
 import "time"
 
 // PsqlNewsDoc is the struct of news docs stored in PostgreSQL database
-type News struct {
-	ID          int        `gorm:"column:id;primarykey"`
-	UUID        string     `gorm:"column:uuid"`
-	Link        string     `gorm:"column:link"`
-	Title       string     `gorm:"column:title"`
-	Description string     `gorm:"column:description"`
-	CreatedAt   *time.Time `gorm:"column:created_at"`
-	Category    string     `gorm:"column:category"`
-	Authors     []*Author  `gorm:"many2many:news_authors;joinForeignKey:news_id;joinReferences:authors_id"`
+type PsqlNewsDoc struct {
+	ID          int           `gorm:"column:id;primarykey"`
+	UUID        string        `gorm:"column:uuid"`
+	Link        string        `gorm:"column:link"`
+	Title       string        `gorm:"column:title"`
+	Description string        `gorm:"column:description"`
+	CreatedAt   *time.Time    `gorm:"column:created_at"`
+	Category    string        `gorm:"column:category"`
+	Authors     []*PsqlAuthor `gorm:"many2many:news_authors;joinForeignKey:news_id;joinReferences:authors_id"`
 }
 
-func (n *News) TableName() string {
+func (n *PsqlNewsDoc) TableName() string {
 	return "news"
 }
 
-// Author is the struct of authors of news docs
-type Author struct {
+// PsqlAuthor is the struct of authors of news docs
+type PsqlAuthor struct {
 	ID   int    `gorm:"column:id;primarykey"`
 	Name string `gorm:"column:name"`
 }
 
-func (a *Author) TableName() string {
+func (a *PsqlAuthor) TableName() string {
 	return "authors"
 }
 
-// NewsAuthors is the reference table for News and Authors
-type NewsAuthors struct {
+// PsqlNewsAuthors is the reference table for News and Authors
+type PsqlNewsAuthors struct {
 	ID         int `gorm:"column:id;primarykey"`
 	NewsID     int `gorm:"column:news_id"`
 	Authors_ID int `gorm:"column:authors_id"`
 }
 
-func (na *NewsAuthors) TableName() string {
+func (na *PsqlNewsAuthors) TableName() string {
 	return "news_authors"
 }
