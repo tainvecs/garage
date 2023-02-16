52f85dccc24632c7fa3918582becdacd456f5303
Chin-Ho Lin <tainvecs@gmail.com>
2023-02-16 21:13:31 +0800

    refactor: update route and handler

--- apisrv/pkg/api/handler/newshdl/handler.go
+++ apisrv/pkg/api/handler/newshdl/handler.go
@@ -7,20 +7,60 @@ import (
 	"github.com/tainvecs/garage/apisrv/pkg/api/middleware"
 	"github.com/tainvecs/garage/apisrv/pkg/data_access/esdao"
 	"github.com/tainvecs/garage/apisrv/pkg/services/newssvc"
+	"gorm.io/gorm"
 )
 
 // Handler of news docs service
 type Handler struct {
+	GetFunc    GetFunc
 	SearchFunc SearchFunc
 }
 
 // New func creates a news docs service handler
-func New(esDAO *esdao.DataAccessObject) *Handler {
+func New(psqlClient *gorm.DB, esDAO *esdao.DataAccessObject) *Handler {
 
+	svcPsqlDAO := newssvc.NewPsqlDAO(psqlClient)
 	svcESDAO := newssvc.NewESDAO(esDAO)
 
 	return &Handler{
+		GetFunc:    NewGetFunc(svcPsqlDAO),
 		SearchFunc: NewSearchFunc(svcESDAO),
+	}
+}
+
+// Get handles get news docs request
+func (h *Handler) Get() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+
+		// request and params binding
+		reuqest := GetRequest{}
+		if err := c.ShouldBindQuery(&reuqest); err != nil {
+			c.JSON(
+				http.StatusBadRequest,
+				middleware.NewErrorResponse(err),
+			)
+			return
+		}
+
+		// process request
+		response, err := h.GetFunc(
+			c.Request.Context(),
+			&reuqest,
+		)
+		if err != nil {
+			c.JSON(
+				http.StatusInternalServerError,
+				middleware.NewErrorResponse(err),
+			)
+			return
+		}
+
+		// response
+		c.JSON(
+			http.StatusOK,
+			middleware.NewResponse(response),
+		)
 	}
 }
 
