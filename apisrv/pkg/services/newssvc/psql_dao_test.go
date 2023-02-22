b9fc4d23ed693e7b68b62b0901d69270227fa4e5
Chin-Ho, Lin <tainvecs@gmail.com>
2023-02-22 23:31:39 +0800

    apisrv: feat/psql-unit-test (#52)
    * chore: update env for psql
    
    * feat: add psql set up script for unit test
    
    * refactor: update es test to init dao once
    
    * refactor: update psql dao only init once and merge unit test
    
    * feat: update ci runner for psql unit test

--- apisrv/pkg/services/newssvc/psql_dao_test.go
+++ apisrv/pkg/services/newssvc/psql_dao_test.go
@@ -1,10 +1,14 @@
 package newssvc_test
 
 import (
+	"context"
 	"errors"
 	"os"
 	"strings"
+	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/tainvecs/garage/apisrv/pkg/data_access/sqldao"
 	"github.com/tainvecs/garage/apisrv/pkg/services/newssvc"
 
 	"gorm.io/driver/postgres"
@@ -13,7 +17,7 @@ import (
 
 var testPsqlDAO newssvc.PsqlDAO
 
-func init() {
+func initPsql() {
 
 	if os.Getenv("TEST_REAL") != "true" {
 		return
@@ -37,4 +41,40 @@ func init() {
 	// new dao
 	dao := newssvc.NewPsqlDAO(client)
 	testPsqlDAO = dao
+}
+
+func TestPsqlDAOReal(t *testing.T) {
+
+	if os.Getenv("TEST_REAL") != "true" {
+		t.Skip()
+	}
+
+	initPsql()
+
+	t.Run("subtestPsqlDAOGetReal", subtestPsqlDAOGetReal)
+}
+
+func subtestPsqlDAOGetReal(t *testing.T) {
+
+	ctx := context.Background()
+
+	// query config
+	config := sqldao.QueryConfig{
+		Fields: []string{"id", "uuid", "link", "title"},
+		Offset: 10,
+		Limit:  10,
+		PreloadAssociations: []string{
+			newssvc.PsqlNewsAuthorsAssociation,
+		},
+	}
+
+	// run get all
+	docs, err := testPsqlDAO.Get(ctx, &config)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, docs)
+
+	// // print debug
+	// mar, err := json.MarshalIndent(docs, "", "\t")
+	// assert.NoError(t, err)
+	// fmt.Println(string(mar))
 }
